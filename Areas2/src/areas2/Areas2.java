b3b83174ed0f86781d8e3c505ab4e41a4adba061
holt145 <30914430+holt145@users.noreply.github.com>
2018-02-13 23:05:23 -0600

    Actividad areas
    Javier Yuriar 
    IDS
     2769454

---
+++ Areas2/src/areas2/Areas2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package areas2;
+
+/**
+ *
+ * @author holt1
+ */
+public class Areas2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Lobby xyz=new Lobby();
+       xyz.setVisible(true);
+    }
+    
+}
---
+++ Areas2/src/areas2/interfaz.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package areas2;
+
+/**
+ *
+ * @author holt1
+ */
+public interface interfaz {
+   public double area = 0;
+}
